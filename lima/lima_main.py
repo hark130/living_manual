c2034ab82c982aa913f4f2df3385503303ba9779
hark130 <hark130@yahoo.com>
2022-10-05 12:27:27 -0500

    LIMA-1: Now parsing arguments
    Argument parsing appears to happening in appropriate ways.
    
    Changes to be committed:
    	new file:   lima/lima_args.py
    	modified:   lima/lima_main.py
    	new file:   lima/lima_validation.py

--- lima/lima_main.py
+++ lima/lima_main.py
@@ -1,7 +1,18 @@
+"""LIVING MANUAL (LIMA) entry point.
+
+    Typical usage example:
+
+    from lima.lima_main import execute
+
+    if __name__ == '__main__':
+        execute()
+"""
+
 # Standard Imports
 import sys
 # Third Party Imports
 # Local Imports
+from lima.lima_args import ARG_DICT_KEY_FILE, ARG_DICT_KEY_WORDS, parse_lima_args
 
 
 def execute() -> None:
@@ -10,12 +21,24 @@ def execute() -> None:
     except Exception as err:
         print(f'ERROR: {str(err)}')
         sys.exit(2)  # Failure
+    # sys.exit(main())
 
 
 def main() -> int:
     # LOCAL VARIABLES
     exit_code = 0  # 0 on success, 1 for bad input, 2 on exception
+    arg_dict = {}  # Dictionary of command line arguments
 
+    # PARSE ARGS
+    try:
+        arg_dict = parse_lima_args()
+    except (FileNotFoundError, OSError, TypeError, ValueError) as err:
+        print(f'ERROR: {str(err)}')
+        exit_code = 1
+    else:
+        print(f'ARG DICT: {arg_dict}')  # DEBUGGING
+        print(f'{ARG_DICT_KEY_FILE} is {arg_dict[ARG_DICT_KEY_FILE]}')  # DEBUGGING
+        print(f'{ARG_DICT_KEY_WORDS} is {arg_dict[ARG_DICT_KEY_WORDS]}')  # DEBUGGING
 
     # DONE
     return exit_code
